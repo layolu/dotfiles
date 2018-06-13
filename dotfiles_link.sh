f381136dbe83317b636c5606620271024ad6bd70
layolu layole <layolulayole@gmail.com>
2018-06-14 01:03:35 +0900

    add .vimrc and python.vim

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,3 +4,8 @@ ln -sf ~/dotfiles/.abcde.conf ~/.abcde.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/config.json ~/.config/powerline-shell/
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+if [ ! -d ~/.vim/ftplugin ]; then
+	mkdir -p ~/.vim/ftplugin
+fi
+ln -sf ~/dotfiles/python.vim ~/.vim/ftplugin/python.vim
