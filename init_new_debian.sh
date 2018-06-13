8dcfbc5a590707bfb1b770feae76a49ff6b0c6bc
layolu layole <layolulayole@gmail.com>
2018-06-14 01:46:42 +0900

    init_new_debian.sh: setup locale ja_JP.UTF-8

--- init_new_debian.sh
+++ init_new_debian.sh
@@ -6,6 +6,10 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
+echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+update-locale LANG=ja_JP.UTF-8
+
 apt update
 apt install -y vim python3 python3-pip python3-venv
 pip3 install -r py3_requirements.txt
