1d57776f210caa6ca12037151f886eddaeb2887e
layolu layole <layolulayole@gmail.com>
2018-06-13 15:57:14 +0900

    first commit

---
+++ dotfiles_link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# https://qiita.com/okamos/items/7f5461814e8ed8916870
+ln -sf ~/dotfiles/.abcde.conf ~/.abcde.conf
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.profile ~/.profile
+ln -sf ~/dotfiles/config.json ~/.config/powerline-shell/
