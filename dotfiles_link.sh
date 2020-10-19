fe38f452af350c036576453c62e6d47f53ed9f80
layolu layole <layolulayole@gmail.com>
2020-10-19 09:29:27 +0900

    added .gitconfig & more safer with backup

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 # https://qiita.com/okamos/items/7f5461814e8ed8916870
-ln -sf ~/dotfiles/.abcde.conf ~/.abcde.conf
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.profile ~/.profile
-if [ ! -d ~/.config/powerline-shell ]; then
-	mkdir -p ~/.config/powerline-shell
-fi
-ln -sf ~/dotfiles/config.json ~/.config/powerline-shell/
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-if [ ! -d ~/.vim/ftplugin ]; then
-	mkdir -p ~/.vim/ftplugin
-fi
-ln -sf ~/dotfiles/python.vim ~/.vim/ftplugin/python.vim
+for f in .abcde.conf .bashrc .gitprofile .profile .vimrc
+	[ -f ~/$f ] && cp -p ~/$f ~/$f.bak.$(date +%s)
+	ln -sf $PWD/$f ~/$f
+
+PS_DIR=~/.config/powerline-shell
+[ ! -d $PS_DIR ] && mkdir -p ~/.config/powerline-shell
+PS_CONFIG=$PS_DIR/config.json
+[ -f $PS_CONFIG ] && cp -p $PS_CONFIG $PS_CONFIG.bak.$(date +%s)
+ln -sf $PWD/config.json $PS_CONFIG
+
+VIM_FTPLUGIN_DIR=~/.vim/ftplugin
+[ ! -d $VIM_FTPLUGIN_DIR ] && mkdir -p $VIM_FTPLUGIN_DIR
+PYTHON_VIM=$VIM_FTPLUGIN_DIR/python.vim
+[ -f $PYTHON_VIM ] && cp -p $PYTHON_VIM $PYTHON_VIM.bak.$(date +%s)
+ln -sf $PWD/python.vim $PYTHON_VIM
